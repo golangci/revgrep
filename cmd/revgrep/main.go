41a660efb064e2adbe36a551cc0da43de77258d0
Corver Roos <corver@luno.com>
2019-12-06 10:49:01 +0200

    Fix go mod for this repo

--- cmd/revgrep/main.go
+++ cmd/revgrep/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/golangci/revgrep"
+	"github.com/corverroos/revgrep"
 )
 
 func main() {
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/golangci/revgrep
+module github.com/corverroos/revgrep
 
 go 1.13
